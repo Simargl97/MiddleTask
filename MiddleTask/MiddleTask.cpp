29f19268f1fba5cfc5ede9f5c45a5540ae829508
sergie <simargll1997@gmail.com>
2025-03-23 22:46:07 +0300

    homework 3.2 фикс фикса

--- MiddleTask/MiddleTask.cpp
+++ MiddleTask/MiddleTask.cpp
@@ -1,63 +1,92 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
+#include <cassert>
 
 // Константный forward-итератор для массива элементов типа T
 // LegacyForwardIterator
 
 template <typename T>
-class ConstForwIter {
+class ConstForwIter
+{
 public:
-    // Категория итератора
     using iterator_category = std::forward_iterator_tag;
-    // Тип значения
     using value_type = T;
-    // Разница между итераторами
     using difference_type = std::ptrdiff_t;
-    // Указатель на константное значение
     using pointer = const T*;
-    // Константная ссылка на значение
-    using reference = const T&; 
+    using reference = const T&;
 
-    // Конструктор принимает указатель на элемент
+    // Конструкторы
+    ConstForwIter() : m_ptr(nullptr) {}  // Конструктор по умолчанию
     explicit ConstForwIter(pointer ptr) : m_ptr(ptr) {}
     
     // Оператор разыменования
-    reference operator*() const { return *m_ptr; }
-    // Оператор доступа к членам структуры/класса через итератор
+    reference operator*() const { assert(m_ptr); return *m_ptr; }
     pointer operator->() const { return m_ptr; }
-
-    // Префиксный инкремент: сначала увеличивает указатель, затем возвращает ссылку на себя
+    
+    // Префиксный и постфиксный инкремент
     ConstForwIter& operator++() { ++m_ptr; return *this; }
-    // Постфиксный инкремент: создаёт копию, увеличивает указатель, возвращает копию
     ConstForwIter operator++(int) { ConstForwIter temp = *this; ++m_ptr; return temp; }
     
     // Операторы сравнения
     bool operator==(const ConstForwIter& other) const { return m_ptr == other.m_ptr; }
     bool operator!=(const ConstForwIter& other) const { return m_ptr != other.m_ptr; }
+    
+    // Операторы сложения и вычитания (для std::advance, std::distance)
+    ConstForwIter operator+(difference_type n) const { return ConstForwIter(m_ptr + n); }
+    ConstForwIter operator-(difference_type n) const { return ConstForwIter(m_ptr - n); }
+    
+    // Разница между итераторами
+    difference_type operator-(const ConstForwIter& other) const { return m_ptr - other.m_ptr; }
+    
+    // Продвигаем итератор вперёд (std::advance)
+    ConstForwIter& operator+=(difference_type n) { m_ptr += n; return *this; }
 
 private:
-    pointer m_ptr; // Указатель на текущий элемент
+    pointer m_ptr;
 };
+
+// std::distance для ConstForwIter
+template <typename T>
+typename ConstForwIter<T>::difference_type distance(ConstForwIter<T> first, ConstForwIter<T> last)
+{
+    return last - first;
+}
+
+// std::advance для ConstForwIter
+template <typename T>
+void advance(ConstForwIter<T>& it, typename ConstForwIter<T>::difference_type n)
+{
+    it += n;
+}
 
 int main()
 {
     setlocale(LC_ALL, "ru_RU.UTF-8");
-
+    
+    
     // Исходный вектор
     std::vector<int> vec = {1, 2, 3, 4, 5};
-    // Итераторы на начало и конец
-    ConstForwIter<int> begin(vec.data()), end(vec.data() + vec.size()); 
     
-    // Перебираем элементы с помощью итератора
-    while (begin != end) {
-        std::cout << *begin << " "; 
-        ++begin; 
+    // Итераторы
+    ConstForwIter<int> begin(vec.data()), end(vec.data() + vec.size());
+    while (begin != end)
+    {
+        std::cout << *begin << std::endl;
+        ++begin;
     }
-    std::cout << std::endl;
+    
+    ConstForwIter<int> beginD(vec.data()), endD(vec.data() + vec.size());
+    // Используем std::distance
+    std::cout << "Расстояние между begin и end: " << distance(beginD, endD) << std::endl;
+    
+    // Используем std::advance
+    advance(beginD, 2);
+    std::cout << "Элемент после сдвига на 2 позиции: " << *beginD << std::endl;
     
     return 0;
 }
+
 
 
 
